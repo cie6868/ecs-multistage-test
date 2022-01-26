ef6ca8e8d4b4872ecfe3eaf9a19e6b4c6733a3ea
Chamath Ellawala <chamath@vetstoria.com>
2022-01-26 16:17:25 +0530

    App changes.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+echo "Version: ${APP_NAME}"
+
+echo "Updating app version in code"
+ROOT_DIR=/usr/share/nginx/html
+for file in $ROOT_DIR/static/js/app.*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
+do
+  echo "Processing $file ...";
+
+  sed -i "s|__APP_NAME|${APP_NAME}|g" $file
+
+done
+
 echo 'Starting nginx...'
 nginx -g 'daemon off;'
