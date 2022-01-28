1b24ffc6bb25ccb829c96dfec19e7724ac363cd8
Chamath Ellawala <chamath@vetstoria.com>
2022-01-28 11:33:48 +0530

    Minor change.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ for file in $ROOT_DIR/static/js/*.js* $ROOT_DIR/index.html;
 do
   echo "Processing $file ...";
 
-  sed -i "s|__APP_NAME|ECS 2|g" $file
+  sed -i "s|__APP_NAME|ECS|g" $file
 done
 
 echo 'Starting nginx...'
